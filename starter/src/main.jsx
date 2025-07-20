dd822b6d49ae825fbf931ac46151621f63b692da
prateekbadjatya <prateek.badjatya@gmail.com>
2025-07-21 02:39:32 +0530

    add rect query devtools

--- starter/src/main.jsx
+++ starter/src/main.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <QueryClientProvider client={queryClient}>
-        <App />
-    </QueryClientProvider>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+    <ReactQueryDevtools initialIsOpen={true} />
+  </QueryClientProvider>
 );
